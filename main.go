a3823d4dedd756a38243c0f885544cc3277b2381
Jay R. Wren <jrwren@xmtp.net>
2017-05-30 10:09:18 -0400

    impl num times

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ var (
 func main() {
 	container := flag.String("c", "", "container name")
 	oname := flag.String("o", "", "object name")
+	n := flag.Int("n", 1, "number of times to do each operation")
 	flag.Parse()
 	if *oname == "" || *container == "" {
 		log.Print("use -o and -c")
@@ -28,8 +29,7 @@ func main() {
 	// don't validate ssl, just testing.
 	//c := client.NewNonValidatingClient(&identity.Credentials{
 	c := client.NewClient(&identity.Credentials{
-		//		URL:     "http://10.0.5.171:8080/auth/v1.0",
-		URL:     "http://127.0.0.1:8080/auth/v1.0",
+		URL:     "http://10.0.5.171:8080/auth/v1.0",
 		User:    "admin:admin",
 		Secrets: "admin",
 		//		TenantName: "admin", // or UserDomain, or ProjectDomain and docs are useless.
@@ -39,22 +39,28 @@ func main() {
 	s := swift.New(c)
 	log.Print("reading ", *container, *oname, "with Reader")
 	start := time.Now()
-	req2, _, err := s.GetReadHandle(*container, *oname)
-	if err != nil {
-		log.Print("failed to get handle to ", *oname, err)
+	for i := 0; i < *n; i++ {
+		req2, _, err := s.GetReadHandle(*container, *oname)
+		if err != nil {
+			log.Print("failed to get handle to ", *oname, err)
+		}
+		_, err = ioutil.ReadAll(req2)
+		// always 8k blocks from Discard/ReadFrom
+		//_, err = ioutil.Discard.(io.ReaderFrom).ReadFrom(req2)
+		if err != nil {
+			log.Print("failed to read handle for README.md:", err)
+		}
 	}
-	_, err = ioutil.ReadAll(req2)
-	if err != nil {
-		log.Print("failed to read handle for README.md:", err)
-	}
-	log.Print("done in ", time.Now().Sub(start)/time.Millisecond, "ms")
+	log.Print("done in ", time.Now().Sub(start))
 	log.Print("reading one request")
 	start = time.Now()
-	_, err = s.GetObject(*container, *oname)
-	if err != nil {
-		log.Print("failed to get handle to ", *oname, err)
+	for i := 0; i < *n; i++ {
+		_, err := s.GetObject(*container, *oname)
+		if err != nil {
+			log.Print("failed to get handle to ", *oname, err)
+		}
 	}
-	log.Print("done in ", time.Now().Sub(start)/time.Millisecond, "ms")
+	log.Print("done in ", time.Now().Sub(start))
 
 	return
 }
